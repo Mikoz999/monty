fe1d7bb7686cc896b066298a30eb5b69ef68ba05
Mikoz999 <mikozotieno@gmail.com>
2023-08-24 11:52:08 -0700

    Add swap function for swapping top two elements in the stack

---
+++ swap.c
@@ -0,0 +1,23 @@
+#include "monty.h"
+
+/**
+ * swap - Swaps the top two elements of the stack
+ * @stack: Double pointer to the stack
+ * @line_number: Line number
+ *
+ * Return: Nothing
+ */
+void swap(stack_t **stack, unsigned int line_number)
+{
+	
+	int temp = (*stack)->n;
+	
+	if (*stack == NULL || (*stack)->next == NULL)
+	{
+		fprintf(stderr, "L%u: can't swap, stack too short\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	
+	(*stack)->n = (*stack)->next->n;
+	(*stack)->next->n = temp;
+}
