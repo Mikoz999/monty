9b7aa84284ad9b3a6e040afbad72d6009655645f
Mikoz999 <mikozotieno@gmail.com>
2023-08-24 12:02:20 -0700

    Add nop function as a placeholder

---
+++ nop.c
@@ -0,0 +1,14 @@
+#include "monty.h"
+
+/**
+ * nop - Does nothing
+ * @stack: Double pointer to the stack
+ * @line_number: Line number
+ *
+ * Return: Nothing
+ */
+void nop(stack_t **stack, unsigned int line_number)
+{
+    (void)stack;
+    (void)line_number;
+}
